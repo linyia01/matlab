5adc44a9e413ed96f24c82063f227a09e43449cc
linyia01 <1393997412@qq.com>
2018-06-04 14:44:39 +0800

    Update ecg_frequency_analysis_filter.m

--- ecg_frequency_analysis_filter.m
+++ ecg_frequency_analysis_filter.m
@@ -1,6 +1,6 @@
 path_str='E:\works\ST-T\心房颤动\';
 listing = dir(path_str);
-d=fdesign.lowpass('N,F3dB',5,40,1000)
+d=fdesign.lowpass('N,F3dB',5,40,1000) %定义滤波
 Hd=design(d,'butter');
 for filename ={listing.name}
     namestr=cell2mat(filename);
@@ -9,16 +9,16 @@ for filename ={listing.name}
         figure(1);
         plot(stdata(2,:));
         DATA1=stdata(2,:)
-        [fl,yl]=fft_simple(DATA1,1000,0,180);
+        [fl,yl]=fft_simple(DATA1,1000,0,180); %频谱分析
         figure(2);
         plot(fl,yl,'r','LineWidth',2);
         xlabel('Frequency/Hz');ylabel('Amplitude')
-        y=filter(Hd,DATA1)
-        save([path_str,namestr,'.mat'],'y')
+        y=filter(Hd,DATA1)  %滤波
+        save([path_str,namestr,'.mat'],'y') %保存滤波结果
         figure(3);
         plot(y);
         DATA1=y
-        [fl,yl]=fft_simple(DATA1,1000,0,180);
+        [fl,yl]=fft_simple(DATA1,1000,0,180); %滤波后的频谱分析
         figure(4);
         plot(fl,yl,'r','LineWidth',2);
         xlabel('Frequency/Hz');ylabel('Amplitude')
